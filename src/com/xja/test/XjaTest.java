c87851d286bb6eb6e49b17517ab0e5c01be53637
xiaosheng <671846152@qq.com>
2021-01-26 20:12:08 +0800

    test

--- src/com/xja/test/XjaTest.java
+++ src/com/xja/test/XjaTest.java
@@ -2,6 +2,7 @@ package com.xja.test;
 
 public class XjaTest {
     public static void main(String[] args){
+        System.out.println("ssssssbbdddd");
         System.out.println("这是我的好东西");
 
     }

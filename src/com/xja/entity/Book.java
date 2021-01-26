7dc6316e8b95b76a5b3d675e50df3ee70987c72f
xiaosheng <671846152@qq.com>
2021-01-26 20:13:38 +0800

    test

---
+++ src/com/xja/entity/Book.java
@@ -0,0 +1,22 @@
+package com.xja.entity;
+
+public class Book {
+    private String bid;
+    private String bname;
+
+    public String getBid() {
+        return bid;
+    }
+
+    public void setBid(String bid) {
+        this.bid = bid;
+    }
+
+    public String getBname() {
+        return bname;
+    }
+
+    public void setBname(String bname) {
+        this.bname = bname;
+    }
+}
